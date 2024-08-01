3b2a75782557bbf708a6703ec75c00bb1087aef5
NAKATA <cristianeynr@gmail.com>
2024-08-01 15:43:40 +0100

    Updated links for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,9 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-import { whyframe } from '@whyframe/core';
-import { whyframeAstro } from '@whyframe/astro';
 import tailwind from "@astrojs/tailwind";
 
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  vite: {
-    plugins: [
-    // Initialize core plugin
-    whyframe({
-      defaultSrc: '/frames/default' // provide our own html
-    }),
-    // Initialize Astro integration plugin
-    whyframeAstro({
-      // Render `iframe`s as Svelte components by default,
-      // can be changed via `data-why="vue"`
-      defaultFramework: 'svelte'
-    })]
-  }
+  site: 'https://crisynr.github.io',
+  base: '/hotel',
 });
