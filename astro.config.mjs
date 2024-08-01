24505dad6163b363dfb1e0870b822cae2a8efcb5
NAKATA <cristianeynr@gmail.com>
2024-08-01 17:26:29 +0100

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   site: 'https://crisynr.github.io',
   base: '/hotel',
   outDir: 'public',
   publicDir: 'static',
+  output: "server",
+  adapter: netlify()
 });
