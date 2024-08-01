9b053d5c262236a5a273435c2ca422a7b4fd319a
Cristiane Nakata <91732258+crisynr@users.noreply.github.com>
2024-08-02 00:16:53 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   site: 'https://crisynr.github.io',
-  base: '/hotel',
-  output: 'static'
+  base: 'hotel',
 });
