ed17790a3a0452ac77390ae125fe26dcc6d81fb0
NAKATA <cristianeynr@gmail.com>
2024-08-02 00:29:37 +0100

    index

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   site: 'https://crisynr.github.io',
-  base: 'hotel',
+  base: '/hotel',
 });
